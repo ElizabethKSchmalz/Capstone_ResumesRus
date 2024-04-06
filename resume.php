88acf4cb4622d6402075b6a3a176756acad660ba
Aidan Niblett <aniblett2@student.cvtc.edu>
2024-04-05 22:34:04 -0500

    Added basic HTML + basic formatting CSS

---
+++ resume.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Resumes R Us</title>
+</head>
+<body>
+
+  <div class="wrapper">
+
+    <div class="sidebar">
+      <a href="index.php"><img src="Images/Logo.png" alt="Resumes R Us Logo"></a>
+      <a href="resume.php">New Resume</a>
+      <a href="#">Thesaurus</a>
+      <a href="#">Ideas for artful jobs</a>
+      <a href="templates.php">Templates</a>
+      <a href="#">Job Ideas</a>
+    </div>
+
+    <div class="header">
+      <a href="#">Login</a>
+      <a href="#">Make a Change</a>
+      <a href="#">Tips</a>
+      <a href="#">Cover Letter</a>
+    </div>
+
+    <div class="mainContent">
+      
+      <h1>RESUME BUILDER</h1>
+      <!-- INSERT RESUME BUILDER CODE HERE -->
+
+    </div>
+
+
+  </div>
+
+</body>
+</html>
