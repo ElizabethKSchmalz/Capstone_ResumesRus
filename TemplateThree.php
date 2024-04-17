579202e32b5cd1b7a9beafc9bc258a1d598f56a6
ElizabethKSchmalz <143556502+ElizabethKSchmalz@users.noreply.github.com>
2024-04-16 21:53:30 -0500

    Add files via upload

---
+++ TemplateThree.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Resumes R Us</title>
+</head>
+<body>
+
+  <div class="wrapper">
+
+  <div class="sidebar">
+      <a href="index.php"><img src="Images/Logo_1.png" alt="Resumes R Us Logo"></a>
+      <br>
+      <br>
+      <br>
+      <a href="resume.php">New Resume</a>
+      <br>
+      <br>
+      <a href="#">Thesaurus</a>
+      <br>
+      <br>
+      <a href="#">Ideas for artful jobs</a>
+      <br>
+      <br>
+      <a href="templates.php">Templates</a>
+      <br>
+      <br>
+      <a href="#">Job Ideas</a>
+    </div>
+
+    <div class="header">
+      <a href="index.php"><img src="Images/Logo_2.png" alt="Resumes R Us Logo"></a>
+      <br>
+      <a href="login.php">Login</a>
+      <a href="#">Make a Change</a>
+      <a href="#">Tips</a>
+      <a href="#">Cover Letter</a>
+    </div>
+
+    <div class="mainContent">
+
+      <h1>TEMPLATES</h1>
+      <a href="/Resume3/Petresume.html">Click here</a>
+    </div>
+
+  </div>
+
+</body>
+</html>
