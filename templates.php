3400b99d036153a05c39ab7dbbca63dc4684a615
Aidan Niblett <aniblett2@student.cvtc.edu>
2024-04-26 21:20:48 -0500

    Modified CSS and structure

--- templates.php
+++ templates.php
@@ -10,20 +10,52 @@
 
   <div class="wrapper">
 
-    <div class="sidebar">
-      <a href="index.php"><img src="Images/Logo.png" alt="Resumes R Us Logo"></a>
+  <div class="sidebar">
+      <a href="index.php"><h1>Resumes R Us</h1></a>
+      <br>
+      <br>
+      <br>
       <a href="resume.php">New Resume</a>
-      <a href="#">Thesaurus</a>
-      <a href="#">Ideas for artful jobs</a>
+      <br>
+      <br>
       <a href="templates.php">Templates</a>
-      <a href="#">Job Ideas</a>
-    </div>
-
-    <div class="header">
-      <a href="#">Login</a>
+      <br>
+      <br>
+      <a href="coverLetter.php">Cover Letter</a>
+      <br>
+      <br>
+      <a href="#">Thesaurus</a>
+      <br>
+      <br>
+      <a href="tips.php">Tips</a>
+      <br>
+      <br>
+      <a href="ideas.php">Job Ideas</a>
+      <br>
+      <br>
+      <a href="#">Ideas for artful jobs</a>
+      <br>
+      <br>
       <a href="#">Make a Change</a>
-      <a href="#">Tips</a>
-      <a href="#">Cover Letter</a>
+
+      <div>
+        <?php
+          session_start(); 
+
+          if (isset($_SESSION['authenticated']) && $_SESSION['authenticated'] === true) {
+              // User is logged in
+              ?>
+              <a href="logout.php">Logout</a> 
+              <?php
+          } else {
+              // User is not logged in 
+              ?>
+              <a href="login.php">Login&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;</a>
+              <a href="signup.php">Sign Up</a>
+              <?php
+          }
+        ?>
+      </div>
     </div>
 
     <div class="mainContent">
